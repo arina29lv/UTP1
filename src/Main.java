7ff75da7690d885b265221dd350165ce04638a01
arina29lv <arinaliubas@gmail.com>
2024-10-08 10:44:01 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Substructor substructor = new Substructor();
+
+        System.out.println(substructor.substruct(6, 3));
+    }
+}
